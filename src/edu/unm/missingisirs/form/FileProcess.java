dc09360cf6f20bb48816017402817b2a53d206f4
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-03-09 15:01:31 -0600

    Added to pass aidYear in the request

---
+++ src/edu/unm/missingisirs/form/FileProcess.java
@@ -0,0 +1,15 @@
+package edu.unm.missingisirs.form;
+
+
+public class FileProcess {
+	private String aidYear;
+
+	public String getAidYear() {
+		return aidYear;
+	}
+
+	public void setAidYear(String aidYear) {
+		this.aidYear = aidYear;
+	}
+
+}
